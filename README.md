74c148614de8564264b7e4c46a7bcdc92ac915a0
rodrigo-diaz <rodrigohdiaz@gmail.com>
2021-05-17 13:35:20 +1200

    update website

--- README.md
+++ README.md
@@ -1,13 +1,54 @@
-# gatsby_blog
+<p align="center">
+  <a href="https://www.gatsbyjs.com/?utm_source=starter&utm_medium=readme&utm_campaign=minimal-starter">
+    <img alt="Gatsby" src="https://www.gatsbyjs.com/Gatsby-Monogram.svg" width="60" />
+  </a>
+</p>
+<h1 align="center">
+  Gatsby minimal starter
+</h1>
 
-What is this?
-This is a new blog where I'll be mergin some of the other projects that I have been
-workin on. A little sample of my side projects and a place to showcase what I can do.
+## 🚀 Quick start
 
-What can you do here?
-You can give me a feedback of anything I could improve.  If it is about any of the
-projects, you probably can find those as a repository in my profile, so feel free to colaborate on those.
+1.  **Create a Gatsby site.**
 
-I appreciate you come and have a look, hope you like what you see.
+    Use the Gatsby CLI to create a new site, specifying the minimal starter.
 
+    ```shell
+    # create a new Gatsby site using the minimal starter
+    npm init gatsby
+    ```
 
+2.  **Start developing.**
+
+    Navigate into your new site’s directory and start it up.
+
+    ```shell
+    cd my-gatsby-site/
+    npm run develop
+    ```
+
+3.  **Open the code and start customizing!**
+
+    Your site is now running at http://localhost:8000!
+
+    Edit `src/pages/index.js` to see your site update in real-time!
+
+4.  **Learn more**
+
+    - [Documentation](https://www.gatsbyjs.com/docs/?utm_source=starter&utm_medium=readme&utm_campaign=minimal-starter)
+
+    - [Tutorials](https://www.gatsbyjs.com/tutorial/?utm_source=starter&utm_medium=readme&utm_campaign=minimal-starter)
+
+    - [Guides](https://www.gatsbyjs.com/tutorial/?utm_source=starter&utm_medium=readme&utm_campaign=minimal-starter)
+
+    - [API Reference](https://www.gatsbyjs.com/docs/api-reference/?utm_source=starter&utm_medium=readme&utm_campaign=minimal-starter)
+
+    - [Plugin Library](https://www.gatsbyjs.com/plugins?utm_source=starter&utm_medium=readme&utm_campaign=minimal-starter)
+
+    - [Cheat Sheet](https://www.gatsbyjs.com/docs/cheat-sheet/?utm_source=starter&utm_medium=readme&utm_campaign=minimal-starter)
+
+## 🚀 Quick start (Gatsby Cloud)
+
+Deploy this starter with one click on [Gatsby Cloud](https://www.gatsbyjs.com/cloud/):
+
+[<img src="https://www.gatsbyjs.com/deploynow.svg" alt="Deploy to Gatsby Cloud">](https://www.gatsbyjs.com/dashboard/deploynow?url=https://github.com/gatsbyjs/gatsby-starter-minimal)
--- src/components/layout.js
+++ src/components/layout.js
@@ -5,6 +5,7 @@ import "../styles/global.scss"
 import logo from "../images/logoR.png"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Nav } from "react-bootstrap";
+import Media from "../components/media"
 
 const ListLink = props => (
   <li style={{className:"nav"}}>
@@ -15,12 +16,13 @@ const ListLink = props => (
 export default function Layout ({ children }) {
     return (
       <header>
-        <Nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <div className="logo">
-              <ListLink to="/">
-                  <img src={logo} alt="Logo" className="logo"/>
-              </ListLink>
-          </div>
+        <div className="logo">
+                <ListLink to="/">
+                    <img src={logo} alt="Logo" className="logo"/>
+                </ListLink>
+        </div>
+
+        <Nav className="navbar navbar-expand-lg navbar-light bg">
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
@@ -33,9 +35,9 @@ export default function Layout ({ children }) {
               <listLink className="nav-item">
                 <a className="nav-link" href="#">About</a>
               </listLink>
-              <listLink className="nav-item">
+              {/* <listLink className="nav-item">
                 <a className="nav-link" href="#">Gallery</a>
-              </listLink>
+              </listLink> */}
               <listLink className="nav-item">
                 <a className="nav-link" href="#">Projects</a>
               </listLink>
@@ -46,21 +48,7 @@ export default function Layout ({ children }) {
         
           </div>
         </Nav>
+        <Media></Media>
         </header>
     )
 }
-
-
-{/* <div>
-        //         <ul className="navbar-nav mr-auto">
-        //           <ListLink to="/" className="nav-item active nav-link">Home</ListLink>
-        //           <ListLink to="/about/" className="nav-item active nav-link">About</ListLink>
-        //           <ListLink to="/gallery/" className="nav-item active nav-link">Gallery</ListLink>
-        //           <ListLink to="/projects/" className="nav-item active nav-link">Projects</ListLink>
-        //           <ListLink to="/contact/" className="nav-item active nav-link">Contact</ListLink>
-        //         </ul>
-        //         </Navbar.Collapse>
-             
-        //       </header>
-        //     {children}
-        // </div> */}
